b8d1e879ff098985b51160070590a0be7baeab34
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-06-19 13:25:47 +1200

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'three-lane': 'repeat(3, minmax(0, 1fr))'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
