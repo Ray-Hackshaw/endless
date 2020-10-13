2e8397a22656321a0db578a204c46acfb8d76b5b
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-13 23:02:00 +1300

    Centering div for chevron.

--- js/new.js
+++ js/new.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
         'opacity': '1',
         'color': 'green;'
     })
-    $('#hidden_btn').append('<img src="assets/icons8-chev.png"></img>').delay(1000)
+    $('#hidden_btn').delay(1000)
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
     $("#endless_title").attr("id", "hidden_title")
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
     })
-
+    
 });
 
