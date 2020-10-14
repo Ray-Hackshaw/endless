d8102b0125b0264b0d665803f053c4ea26b26c57
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-14 21:29:51 +1300

    One click protection and blinking chevrons.

--- js/new.js
+++ js/new.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
-    $('#1_block').click(function()
-    {   
+    $('#1_block').one("click", function()
+    {
+    
+    $("#1_block").attr("id", "newID")
     $('#holder').css({
         'opacity': '1',
         'color': 'green;'
     })
-    $('#hidden_btn').delay(1000)
+    
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
-    $("#endless_title").attr("id", "hidden_title")
+    
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
+    $('body').append('<div class="hid"></div>')
+    setTimeout(function(){
+        $('.hid').append('<div class=""><a id="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a></div>');}, 6500)
+    setTimeout(function(){
+        $('.hid').append('<div class=""><a id="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a></div>');}, 7000)
+    setTimeout(function(){
+        $('.hid').append('<div class=""><a id="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a></div>');}, 7500)
     })
+    
     
 });
 
