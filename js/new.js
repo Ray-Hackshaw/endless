3956b62277262e77f36f35b6424ff0c97594e551
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-15 21:43:02 +1300

    Chevron div changes and starting onclick.

--- js/new.js
+++ js/new.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#1_block').one("click", function()
     {
     
-    $("#1_block").attr("id", "newID")
+    $("#1_block").attr("id", "new_filler_id")
     $('#holder').css({
         'opacity': '1',
         'color': 'green;'
@@ -11,15 +11,22 @@ $(document).ready(function(){
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
     
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
-    $('body').append('<div class="hid"></div>')
     setTimeout(function(){
-        $('.hid').append('<div class=""><a id="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a></div>');}, 6500)
+        $('body').append('<div class="arrows" onclick="toEndless()"></div>');}, 3000) 
     setTimeout(function(){
-        $('.hid').append('<div class=""><a id="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a></div>');}, 7000)
+        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 6500)
     setTimeout(function(){
-        $('.hid').append('<div class=""><a id="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a></div>');}, 7500)
+        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 7000)
+    setTimeout(function(){
+        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 7500)
+    setTimeout(function(){
+        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 8000)
+    setTimeout(function(){
+        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 8500)
     })
-    
-    
 });
+
+function toEndless() {
+    console.log("take me away from here")
+}
 
