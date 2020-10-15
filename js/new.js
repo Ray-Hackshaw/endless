f24b1448a7af8bd0aae0adc3360ac8c06e71f14f
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-15 21:44:35 +1300

    Adding body comments to js file for easy migration

--- js/new.js
+++ js/new.js
@@ -26,7 +26,21 @@ $(document).ready(function(){
     })
 });
 
-function toEndless() {
-    console.log("take me away from here")
-}
+// function toEndless() {
+//     console.log("take me away from here")
+// }
+// <body>
+//     <main class="container py-4 px-4 mx-auto my-12">
+//     </main>
 
+// <script>
+//     $(function() {
+//         console.log("function exec")
+//         $.getJSON('./js/data.json', function(data) {
+//             $.each(data.projects, function(_, e) {
+//                 $('main').append('<div class="project flex flex-wrap -mx-4"><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><h2 class="text-2xl mb-2">' + e.project + '</h2><a href="' + e.link + '"target="_blank">' + e.link + '</a></div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3">' + e.description + '</div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><img src="' + e.imageone + '" alt="Code snippet"><br><img src="' + e.imagetwo + '" alt=""</div></div>');
+//             });
+//         });
+//     });
+// </script>
+// </body>
