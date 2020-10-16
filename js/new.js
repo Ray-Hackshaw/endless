889eb39e8cb095c793d4a1c07dc903db3bd9eea1
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-16 22:52:39 +1300

    Changes to inset box shadows and removing arrows.

--- js/new.js
+++ js/new.js
@@ -2,33 +2,30 @@ $(document).ready(function(){
     $('#1_block').one("click", function()
     {
     
-    $("#1_block").attr("id", "new_filler_id")
+    $("#1_block").attr("id", "#2_block")
     $('#holder').css({
         'opacity': '1',
-        'color': 'green;'
     })
     
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
     
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
-    setTimeout(function(){
-        $('body').append('<div class="arrows" onclick="toEndless()"></div>');}, 3000) 
-    setTimeout(function(){
-        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 6500)
-    setTimeout(function(){
-        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 7000)
-    setTimeout(function(){
-        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 7500)
-    setTimeout(function(){
-        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 8000)
-    setTimeout(function(){
-        $('.arrows').append('<a class="hidden_btn"><img class="blink_me" src="assets/icons8-chev.png"></img></a>');}, 8500)
     })
-});
+    });
 
-// function toEndless() {
-//     console.log("take me away from here")
-// }
+    
+
+    
+function toEndless() {
+    $('#holder').css({
+        'opacity': '0;'
+    });
+    console.log("take me away from here")
+
+
+
+
+}
 // <body>
 //     <main class="container py-4 px-4 mx-auto my-12">
 //     </main>
