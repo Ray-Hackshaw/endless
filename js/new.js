575095346926be15f6fde996eaca14b8b332b15a
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-18 21:46:30 +1300

    Navigation changes and endless content loading.

--- js/new.js
+++ js/new.js
@@ -1,31 +1,64 @@
 $(document).ready(function(){
     $('#1_block').one("click", function()
     {
-    
-    $("#1_block").attr("id", "#2_block")
+
     $('#holder').css({
         'opacity': '1',
     })
-    
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
-    
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
     })
+});
+
+
+    $("#holder").live("click", function(){
+        $(".parentblock").css({
+            "cursor": "default"
+        });
+        $("#1_block").animate({
+            "opacity": "0"
+        }, 2);
+        setTimeout(function() {
+            $("#1_block").remove();
+            $('#endless_title').remove();
+            $('#holder').remove();
+        }, 100);
+        $("#parent").animate({
+            "height": "80%"
+        }, 2000);
+        $("#parent").animate({
+            "width": "80%"
+        }, 2000);
+
+        
+        $("#parent").addClass("scrollable")
+        setTimeout(function() {
+            $.getJSON('./js/data.json', function(data) {
+                $.each(data.projects, function(_, e) {
+                    $('#parent').append('<div class="project flex flex-wrap -mx-4"><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><h2 class="text-2xl mb-2">' + e.project + '</h2><a href="' + e.link + '"target="_blank">' + e.link + '</a></div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3">' + e.description + '</div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><img src="' + e.imageone + '" alt="Code snippet"><br><img src="' + e.imagetwo + '" alt=""</div></div>');
+                });
+        }, 40000);
+        });
+        
     });
 
+
+    //     $(this).css("transition", "all 5s ease-in"),
+    //     $(this).css("width", "70%"); // on hover
+    // }).delay(100).slideUp(100);
+
+
+// function toEndless() {
+//     $('.parentblock').css({
+//         'transition': 'all 2s ease',
+//         'width': '2000px',
+//         'height': '2000px;'
+//     });
+//     console.log("take me away from here")
     
-
-    
-function toEndless() {
-    $('#holder').css({
-        'opacity': '0;'
-    });
-    console.log("take me away from here")
+// }
 
 
-
-
-}
 // <body>
 //     <main class="container py-4 px-4 mx-auto my-12">
 //     </main>
