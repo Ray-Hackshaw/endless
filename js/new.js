908a791f89decb07476f60476b11223a1c6f2d22
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-21 22:38:10 +1300

    Experimenting with darkmode + adding text glow.

--- js/new.js
+++ js/new.js
@@ -17,6 +17,9 @@ $(document).ready(function(){
         $("#home").animate({
             "opacity": "1"
         }, 2000)
+        $("#darkmode").animate({
+            "opacity": "1"
+        }, 2000)
         $("#1_block").animate({
             "opacity": "0"
         }, 2);
@@ -36,7 +39,7 @@ $(document).ready(function(){
         setTimeout(function() {
             $.getJSON('./js/data.json', function(data) {
                 $.each(data.projects, function(_, e) {
-                    $('#parent').append('<div class="project flex flex-wrap -mx-4"><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><h2 id="projectname" class="mb-2">' + e.project + '</h2><a href="' + e.link + '"target="_blank">' + e.link + '</a></div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3">' + e.description + '</div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><img src="' + e.imageone + '" alt="Code snippet"><br><img src="' + e.imagetwo + '" alt=""</div></div>');
+                    $('#parent').append('<div class="project flex flex-wrap -mx-4"><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><h2 id="projectname" class="mb-2 glow">' + e.project + '</h2><a href="' + e.link + '"target="_blank">' + e.link + '</a></div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3">' + e.description + '</div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><img src="' + e.imageone + '" alt="Code snippet"><br><img src="' + e.imagetwo + '" alt=""</div></div>');
                 });
         }, 40000);
         });
