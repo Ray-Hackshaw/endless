ff516464cdbfd40d493700f2a37aae245420543d
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-21 22:47:45 +1300

    Starting on dark mode button functionality.

--- js/new.js
+++ js/new.js
@@ -1,4 +1,15 @@
+function toggleDarkMode() {
+    alert("Working on it - come back soon :) ")
+    // setTimeout(function(){
+    //     $(".parentblock").css({
+    //         "background": "black",
+    //         "color": "white"
+    //     }, 20000);
+    // })
+}
+
 $(document).ready(function(){
+    
     $('#1_block').one("click", function()
     {
 
@@ -8,7 +19,7 @@ $(document).ready(function(){
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
     })
-});
+    });
 
     $("#holder").live("click", function(){
         $(".parentblock").css({
@@ -43,6 +54,5 @@ $(document).ready(function(){
                 });
         }, 40000);
         });
-        
 });
 
