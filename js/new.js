457f64dcef85cbdc2f5dba1c2399f26dd1711585
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2020-10-23 23:17:16 +1300

    Colour changes and text shadow changes. Temporary.

--- js/new.js
+++ js/new.js
@@ -27,8 +27,14 @@ $(document).ready(function(){
     {
 
     $('#holder').css({
-        'opacity': '1',
+        'opacity': '1'
     })
+    $('#endless_title').css({
+        'transition': 'text-shadow 4s ease',
+        'text-shadow': '0 0 2px black;'
+    })
+
+
     $('#endless_title').slideDown(700).delay(700).slideUp(700)
     $('#holder').html('Endless is a catalogue of work and backlog of previous projects, as well as a hub for resources I find useful and/or interesting. I wanted a platform to better explain my ideas and reasoning for personal projects, more than just what lives on Github. I wanted to have everything as a stream of ideas that were all in one place. It is updated periodically with any new projects as well as changes to old work I may have revisited.')
     })
