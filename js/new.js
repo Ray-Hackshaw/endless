ce7d0204a86d87f58f4e298a3ccd1b00c2852634
36725950 <36725950+Ray-Hackshaw@users.noreply.github.com>
2021-01-02 23:24:22 +1300

    New project entry: Celo Health recruitment.

--- js/new.js
+++ js/new.js
@@ -55,9 +55,6 @@ $(document).ready(function(){
         $("#home").animate({
             "opacity": "1"
         }, 2000)
-        // $("#darkmode").animate({
-        //     "opacity": "1"
-        // }, 2000)
         $("#1_block").animate({
             "opacity": "0"
         }, 2);
@@ -77,7 +74,7 @@ $(document).ready(function(){
         setTimeout(function() {
             $.getJSON('./js/data.json', function(data) {
                 $.each(data.projects, function(_, e) {
-                    $('#parent').append('<div class="project flex flex-wrap -mx-4"><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><h2 id="projectname" class="mb-2 glow">' + e.project + '</h2><a href="' + e.link + '"target="_blank">' + e.link + '</a></div><div class="ipad mb-4 md:mb-0 px-4 w-full md:w-1/3">' + e.description + '</div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><img src="' + e.preview + '" alt="preview"><div class="repository"><a href="' + e.repository + '"target="_blank">&lt;source&gt;</a></div></div></div>');
+                    $('#parent').append('<div class="project flex flex-wrap -mx-4"><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><h2 id="projectname" class="mb-2 glow">' + e.project + '</h2><a href="' + e.link + '"target="_blank">' + e.link + '</a></div><div class="ipad mb-4 md:mb-0 px-4 w-full md:w-1/3">' + e.description + '</div><div class="mb-4 md:mb-0 px-4 w-full md:w-1/3"><img src="' + e.preview + '" alt="preview">');
                 });
         }, 40000);
         });
